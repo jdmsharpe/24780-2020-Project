bd09c60a1c08bdfcfcd2f318fd5706dcebfd2267
jdmsharpe <55511821+jdmsharpe@users.noreply.github.com>
2020-11-12 20:29:22 -0500

    First commit

---
+++ OpenGL_on_a_Windows_Form/OpenGL_on_a_Windows_Form.cpp
@@ -0,0 +1,18 @@
+// OpenGL_on_a_Windows_Form.cpp : main project file.
+
+#include "stdafx.h"
+#include "OpenGLViewer.h"
+
+using namespace OpenGL_on_a_Windows_Form;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew OpenGLViewer());
+	return 0;
+}
