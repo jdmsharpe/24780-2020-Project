121e0145ff3be487de8e6517702600735617a333
John Magnus-Sharpe <jdmsharpe@gmail.com>
2020-12-05 23:05:30 -0500

    Updated OpenGL.h to draw objects in pixel units

--- OpenGL_on_a_Windows_Form/ViewManager.cpp
+++ OpenGL_on_a_Windows_Form/ViewManager.cpp
@@ -224,31 +224,12 @@ void ViewManager::draw(vector<Node> Nodes)
 void ViewManager::draw() // Only for testing
 {
 	glLoadIdentity();
-	glTranslatef(0.0f, 0.0f, -10.0f);						// Move left 1.5 units and into the screen 6.0
-	glBegin(GL_TRIANGLES);								// Start drawing a triangle
-	glColor3f(1.0f, 1.0f, 1.0f);						// Red
-	glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (front)
-	glColor3f(0.0f, 1.0f, 0.0f);						// Green
-	glVertex3f(-1.0f, -1.0f, 1.0f);					// Left of triangle (front)
-	glColor3f(0.0f, 0.0f, 1.0f);						// Blue
-	glVertex3f(1.0f, -1.0f, 1.0f);					// Right of triangle (front)
-	glColor3f(1.0f, 0.0f, 0.0f);						// Red
-	glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (right)
-	glColor3f(0.0f, 0.0f, 1.0f);						// Blue
-	glVertex3f(1.0f, -1.0f, 1.0f);					// Left of triangle (right)
-	glColor3f(0.0f, 1.0f, 0.0f);						// Green
-	glVertex3f(1.0f, -1.0f, -1.0f);					// Right of triangle (right)
-	glColor3f(1.0f, 0.0f, 0.0f);						// Red
-	glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (back)
-	glColor3f(0.0f, 1.0f, 0.0f);						// Green
-	glVertex3f(1.0f, -1.0f, -1.0f);					// Left of triangle (back)
-	glColor3f(0.0f, 0.0f, 1.0f);						// Blue
-	glVertex3f(-1.0f, -1.0f, -1.0f);					// Right of triangle (back)
-	glColor3f(1.0f, 0.0f, 0.0f);						// Red
-	glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (left)
-	glColor3f(0.0f, 0.0f, 1.0f);						// Blue
-	glVertex3f(-1.0f, -1.0f, -1.0f);					// Left of triangle (left)
-	glColor3f(0.0f, 1.0f, 0.0f);						// Green
-	glVertex3f(-1.0f, -1.0f, 1.0f);					// Right of triangle (left)
+	glTranslatef(0.0f, 0.0f, 0.0f);				// Move right 1.5 units and into the screen 7.0
+	glColor3f(1.0f, 0.5f, 0.0f);
+	glBegin(GL_QUADS);								// Start drawing a triangle
+	glVertex2f(10.0f, 10.0f);
+	glVertex2f(10.0f, 50.0f);
+	glVertex2f(50.0f, 50.0f);
+	glVertex2f(50.0f, 10.0f);
 	glEnd();											// Done drawing the pyramid
 }
