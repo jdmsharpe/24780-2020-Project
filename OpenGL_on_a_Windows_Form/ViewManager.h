cf5731ef45e9b3916e75952b137a7ac62572401e
robustperf <samkp7@gmail.com>
2020-12-07 19:18:38 -0500

    Added communication between ViewManager and GearWorksGUI

--- OpenGL_on_a_Windows_Form/ViewManager.h
+++ OpenGL_on_a_Windows_Form/ViewManager.h
@@ -11,6 +11,7 @@
 #include <GL/glu.h>
 
 #include <string>
+#include <sstream>
 #include <vector>
 
 #include "Node.h"
@@ -42,10 +43,101 @@ private:
 
 	UnitSystem unitSys;
 
+	struct gearType {
+		double pitch;
+		double pressureAngle;
+		int teeth;
+		int qty;
+		double hubD1, hubD2;
+		HubShape hub;
+	};
+
+	std::vector<gearType> gearSpecifications;
+
 public:
 	ViewManager();
 	
 	bool manage();
+
+
+	// addGear returns -1 if this is a new gear, else returns index of gear in vector 
+	int addGear(double pitch, double pressureAngle, int teeth, int qty, HubShape hub, 
+		double hubD1 = 0, double hubD2 = 0) {
+		gearType thisGear;
+		thisGear.pitch = pitch;
+		thisGear.pressureAngle = pressureAngle;
+		thisGear.teeth = teeth;
+		thisGear.qty = qty;
+		thisGear.hub = hub;
+		thisGear.hubD1 = hubD1;
+		thisGear.hubD2 = hubD2;
+		// add the gear only if it is not already in the list
+		int gearIndex = gearExists(thisGear);
+		if (gearIndex == -1) 
+			gearSpecifications.push_back(thisGear);
+		else 
+			gearSpecifications.at(gearIndex).qty++;
+		return gearIndex;
+	}
+
+	// check if 'thisGear' is already a member of the gearSpecifications vector. If so return index, else -1
+	int gearExists(gearType thisGear) {
+		// don't check if there are no gears in the vector
+		if (gearSpecifications.size() > 0) {
+			// check each gear in the vector, stoping if there is a match
+			for (int i = 0; i < gearSpecifications.size(); i++) {
+				// check if all the properties are the same
+				if (gearSpecifications.at(i).pitch == thisGear.pitch &&
+					gearSpecifications.at(i).pressureAngle == thisGear.pressureAngle &&
+					gearSpecifications.at(i).teeth == thisGear.teeth &&
+					gearSpecifications.at(i).hub == thisGear.hub &&
+					gearSpecifications.at(i).hubD1 == thisGear.hubD1 &&
+					gearSpecifications.at(i).hubD2 == thisGear.hubD2) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	int gearEntries() {
+		return gearSpecifications.size();
+	}
+
+	int getQTY(int i){
+		return gearSpecifications.at(i).qty;
+	}
+
+	void setQTY(int index, int qty) {
+		gearSpecifications.at(index).qty = qty;
+	}
+
+	std::string getLabel(int index) {
+		std::stringstream label;
+		label << "P: ";
+		label << gearSpecifications.at(index).pitch;
+		label << " PA: ";
+		label << gearSpecifications.at(index).pressureAngle;
+		label << " T: ";
+		label << gearSpecifications.at(index).teeth;
+		label << " Hub: ";
+		switch (gearSpecifications.at(index).hub) {
+		case 0:
+			label << "None";
+			break;
+		case 1:
+			label << "Circle, Ø: ";
+			label << gearSpecifications.at(index).hubD1;
+			break;
+		case 2:
+			label << "Rectangle, W: ";
+			label << gearSpecifications.at(index).hubD1;
+			label << " H: ";
+			label << gearSpecifications.at(index).hubD2;
+			break;
+		}
+		return label.str();
+	}
 
 	void getScreenCoords(double modelX, double modelY,
 		double& screenX, double& screenY);
