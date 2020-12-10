198707c3d9c1737d4f9cb377541556fc3c3d3ee7
John Magnus-Sharpe <jdmsharpe@gmail.com>
2020-12-09 23:45:09 -0500

    Exporter now capable of exporting any gear your little heart desires

--- OpenGL_on_a_Windows_Form/Exporter.cpp
+++ OpenGL_on_a_Windows_Form/Exporter.cpp
@@ -8,15 +8,6 @@ void Exporter::exportSVG(ViewManager &aManager, std::string filename)
 	std::vector<gear> gears = aManager.getGearVector();
 	int numGears = (int)gears.size();
 
-	//if (unitSys == imperial) {
-	//	int width = aManager.width * INCH_TO_PX;
-	//	int height = aManager.height * INCH_TO_PX;
-	//}
-	//else if (unitSys == metric) {
-	//	int width = aManager.width * MM_TO_PX;
-	//	int height = aManager.height * MM_TO_PX;
-	//}
-
 	std::pair<double, double> materialDimensions = aManager.getMaterialDimensions();
 
 	Dimensions dimensions(materialDimensions.first * INCH_TO_PX, materialDimensions.second * INCH_TO_PX);
@@ -39,18 +30,17 @@ void Exporter::exportSVG(ViewManager &aManager, std::string filename)
 		double pressureAngle = gears[i].getPR() * (PI / 180.0);
 		std::pair<double, double> offset = { gears[i].getX() * INCH_TO_PX, dimensions.height - gears[i].getY() * INCH_TO_PX };
 
-		double pitchRadius = (numTeeth / pitch) / 2.0 * INCH_TO_PX;
-		double addendum = 1.0 / pitch;
-		double dedendum = 1.25 * addendum;
-		double tooth_depth = addendum + dedendum;
-		double outerRadius = gears[i].getOuter_radius() * INCH_TO_PX;
+		double pitchRadius = ((numTeeth / pitch) / 2.0) * INCH_TO_PX;
+		double addendum = (1.0 / pitch) * INCH_TO_PX;
+		double dedendum = (1.25 / pitch) * INCH_TO_PX;
+		double outerRadius = pitchRadius + addendum;
 
 		// Calculate additional parameters
-		double baseRadius = (pitchRadius - dedendum) * std::cos(pressureAngle);
-		double footRadius = (pitchRadius - dedendum) - pitch / PI;
+		double baseRadius = pitchRadius * std::cos(pressureAngle);
+		double innerRadius = pitchRadius - dedendum;
 
 		double toothMax = std::acos(baseRadius / outerRadius);
-		double toothMin = (footRadius < baseRadius) ? 0 : std::acos(baseRadius / footRadius);
+		double toothMin = (innerRadius < baseRadius) ? 0 : std::acos(baseRadius / innerRadius);
 		double deport = (1 - clearance) * (PI / numTeeth / 2 + std::tan(pressureAngle) - pressureAngle);
 		double topMax = deport - std::tan(toothMax) + toothMax;
 
@@ -61,17 +51,17 @@ void Exporter::exportSVG(ViewManager &aManager, std::string filename)
 
 		// Move to starting point to begin drawing
 		pathObject << PathPoint(outerRadius * std::cos(alpha) + offset.first,
-			outerRadius * std::sin(alpha) + offset.second, "M");
+								outerRadius * std::sin(alpha) + offset.second, "M");
 
 		//Loop through each tooth and draw the path between
 		for (int j = 0; j < numTeeth; j++) {
 
 			double startingAlpha = alpha;
-			alpha += 2 * topMax;
+			alpha += 2.0 * topMax;
 
 			// Draw first arc (outer part of tooth)
 			pathObject << ArcPoint(outerRadius, outerRadius, 0, 0, 1, outerRadius * std::cos(alpha) + offset.first,
-				outerRadius * std::sin(alpha) + offset.second);
+																	  outerRadius * std::sin(alpha) + offset.second);
 
 			alpha += std::tan(toothMax) - toothMax;
 
@@ -79,21 +69,22 @@ void Exporter::exportSVG(ViewManager &aManager, std::string filename)
 			{
 				// Draw first curve to root of gear
 				pathObject << PathPoint(point.second * std::cos(alpha + point.first) + offset.first,
-					point.second * std::sin(alpha + point.first) + offset.second, "L");
+										point.second * std::sin(alpha + point.first) + offset.second, "L");
 			}
 
-			pathObject << PathPoint(footRadius * std::cos(alpha) + offset.first,
-				footRadius * std::sin(alpha) + offset.second, "L");
+			pathObject << PathPoint(innerRadius * std::cos(alpha) + offset.first,
+									innerRadius * std::sin(alpha) + offset.second, "L");
 
 			alpha = startingAlpha + (2 * PI) / numTeeth - deport + topMax;
 
-			pathObject << ArcPoint(footRadius, footRadius, 0, 0, 1, footRadius * std::cos(alpha) + offset.first,
-				footRadius * std::sin(alpha) + offset.second);
+			pathObject << ArcPoint(innerRadius, innerRadius, 0, 0, 1, innerRadius * std::cos(alpha) + offset.first,
+																	innerRadius * std::sin(alpha) + offset.second);
 
-			for (auto const& point : toothProfile) {
+			for (int i = toothProfile.size() - 1; i >= 0; i--) {
 				// Draw last curve to outer part of next tooth
+				std::pair<double, double> point = toothProfile[i];
 				pathObject << PathPoint(point.second * std::cos(alpha - point.first) + offset.first,
-					point.second * std::sin(alpha - point.first) + offset.second, "L");
+										point.second * std::sin(alpha - point.first) + offset.second, "L");
 			}
 
 			alpha = startingAlpha + (2 * PI) / numTeeth;
@@ -123,8 +114,8 @@ void Exporter::exportSVG(ViewManager &aManager, std::string filename)
 std::vector<std::pair<double, double>> Exporter::getToothProfile(double toothMax, double toothMin, double baseRadius)
 {
 	std::vector<std::pair<double, double>> toothProfile;
-	for (int u = 0; u < pointResolution; u++) {
-		double t = (1 - u / pointResolution) * (-toothMax) + (u / pointResolution) * (-toothMin);
+	for (int u = 0; u < pointResolution; u++) { // Change to float
+		double t = (1 - (float)u / pointResolution) * (-toothMax) + ((float)u / pointResolution) * (-toothMin);
 		toothProfile.push_back(std::make_pair(std::tan(t) - t, baseRadius / std::cos(t)));
 	}
 	return toothProfile;
